97b0c30cb546017044799c0eba6df470838597c5
Csaba79-coder <csabavadasz79@gmail.com>
2021-06-05 18:01:26 +0200

    refactor of ArrayList

--- src/list/Main.java
+++ src/list/Main.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        ArrayList<Integer> numbers = new ArrayList<>();
         numbers.add(5);
         numbers.add(9);
         numbers.add(8);
